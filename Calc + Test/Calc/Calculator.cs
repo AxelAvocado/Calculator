cbf8a21bed30e1424b2e88ffce37d4a30a007827
AxelAvocado <abdallah@ajjawi.dk>
2019-08-29 13:15:18 +0200

    Abdallah: Initial project

---
+++ Calc + Test/Calc/Calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Power(double x, double exp)
+        {
+            return Math.Pow(x, exp);
+        }
+
+        private static void Main(string[] args)
+        {
+            var calc = new Calculator();
+
+            Console.WriteLine("5 + 5 = {0}", calc.Add(5, 5));
+            Console.WriteLine("5 - 5 = {0}", calc.Subtract(5, 5));
+            Console.WriteLine("5 * 5 = {0}", calc.Multiply(5, 5));
+            Console.WriteLine("5^5 = {0}", calc.Power(5, 5));
+        }
+    }
+}
